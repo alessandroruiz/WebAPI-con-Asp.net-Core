ed538a44addb6fc57dd6757949715f72f9057dec
alessandroruiz <alessandroruiznina@gmail.com>
2025-06-19 23:56:02 -0400

    agregar el proyecto

--- obj/Release/net8.0/proyecto1.AssemblyInfo.cs
+++ obj/Release/net8.0/proyecto1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyecto1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c52bc3a59c9325ce882797df1e0705d48eb6194")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyecto1")]
